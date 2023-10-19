6105aa7364ebf62f39bbb7ff5e2a58b6a7e97b57
Sjedsson <baqirsyed10@gmail.com>
2023-10-19 14:41:42 +0200

    correceted

--- split-the-nota-exercise/js/index.js
+++ split-the-nota-exercise/js/index.js
@@ -1,1 +1,35 @@
-// Write your code here
+const showSumElem = document.querySelector('#showSum');
+const inputFormElem = document.querySelector('#inputForm');
+const friendSumElem = document.querySelector('#friendSum');
+const calculateButton = document.querySelector('#calculateButton');
+
+function calculateTip(sum, tip) {
+    const calculatedTip = parseInt(sum) * parseFloat(tip);
+    const total = parseInt(sum) + calculatedTip;
+
+    return total;
+}
+
+function divideTotal(total, numberOfFriends) {
+   const sumDivided = total / parseInt(numberOfFriends);
+   return sumDivided;
+   
+}
+
+function showDividedSum(sum) {
+    showSumElem.classList.toggle('hide');
+    inputFormElem.classList.toggle('hide');
+    friendSumElem.innerHTML = sum + ' kr';
+    
+}
+
+calculateButton.addEventListener('click', () => {
+    const tip = document.querySelector('#tip').value;
+    const sum = document.querySelector('#sum').value;
+    const numberOfFriends = document.querySelector('#numberOfFriends').value;
+
+    const total = calculateTip(sum, tip);
+    const friendSum = divideTotal(total, numberOfFriends);
+    showDividedSum(friendSum);
+    
+});
